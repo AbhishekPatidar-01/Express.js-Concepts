ab64c9a0e754ff491c4e2e24abe64432f10fc463
AbhishekPatidar-01 <abhishekpatidar5538@gmail.com>
2025-10-24 15:45:26 +0530

    Learn express.js Concepts

--- app.js
+++ app.js
@@ -1,60 +1,20 @@
 const express = require('express');
 const app = express();
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
-const {products} = require('./data')
+//Static Assets
+app.use(express.static('./methods-public'));
 
-app.get('/' , (req, res) => {
-    res.send('<h1> HomePage </h1><a href = "/api/products">Products</a> ');
-})
+//parse form data
+app.use(express.urlencoded({extended: false}));
 
-app.get('/api/products' ,(req, res) => {
-    const newProduct = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image};
-    })
-    res.json(newProduct);
-})
+//parse json
+app.use(express.json());
 
-app.get('/api/products/:productID' ,(req, res) => {
-    // console.log(req);
-    // console.log(req.params);
-    
-    const {productID} = req.params;  
-
-    const singleProduct = products.find((product) =>  product.id === Number(productID))
-
-    if(!singleProduct){
-        return res.status(404).send("Product is not found");
-    }
-    return res.json(singleProduct);
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    console.log(req.params);
-    res.send('Hello world');
-} )
-
-app.get('/api/v1/query', (req, res) => {
-    
-    // console.log(req.query);
-    const {search, limit} = req.query;
-    let sortedProduct = [...products];
-
-    if(search) {
-        sortedProduct = sortedProduct.filter((product) => {
-            return product.name.startsWith(search);
-        })
-    }
-    if(limit){
-        sortedProduct = sortedProduct.slice(0, Number(limit))
-    }
-    if(sortedProduct.length < 1) {
-        // res.status(200).send('No Product matched your search');
-        return res.status(200).json({success: true, data : []})
-    }
-    return res.status(200).json(sortedProduct);
-} )
-
+app.use('/api/people', people);
+app.use('/login', auth);
+ 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000.... ");
     
---
+++ routes/people.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const  router = express.Router();
+
+const {
+    getPerson,
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletePerson
+} = require('../controllers/people')
+
+
+router.get('/', getPerson)
+router.post('/', createPerson )
+
+router.post('/postman', createPersonPostman )
+
+router.put('/:id', updatePerson )
+
+router.delete('/:id', deletePerson)
+
+module.exports = router;
